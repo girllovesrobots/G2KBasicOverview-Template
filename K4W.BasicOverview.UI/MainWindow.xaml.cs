bfc6415a6af85b83b66b4b4196be751044a4bfd6
girllovesrobots <weiwlow@gmail.com>
2014-10-28 14:47:36 -0400

    Downloaded G2K template
    Template used for tutorial utilizing basic functions on the 2nd Gen.
    Kinect for Windows

---
+++ K4W.BasicOverview.UI/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Windows;
+
+namespace K4W.BasicOverview.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region UI Methods
+
+        private void OnToggleCamera(object sender, RoutedEventArgs e)
+        {
+            ChangeVisualMode("Camera");
+        }
+
+        private void OnToggleDepth(object sender, RoutedEventArgs e)
+        {
+            ChangeVisualMode("Depth");
+        }
+
+        private void OnToggleInfrared(object sender, RoutedEventArgs e)
+        {
+            ChangeVisualMode("Infrared");
+        }
+
+        /// <summary>
+        /// Change the UI based on the mode
+        /// </summary>
+        /// <param name="mode">New UI mode</param>
+        private void ChangeVisualMode(string mode)
+        {
+            // Invis all
+            CameraGrid.Visibility = Visibility.Collapsed;
+            DepthGrid.Visibility = Visibility.Collapsed;
+            InfraredGrid.Visibility = Visibility.Collapsed;
+
+            switch (mode)
+            {
+                case "Camera":
+                    CameraGrid.Visibility = Visibility.Visible;
+                    break;
+
+                case "Depth":
+                    DepthGrid.Visibility = Visibility.Visible;
+                    break;
+
+                case "Infrared":
+                    InfraredGrid.Visibility = Visibility.Visible;
+                    break;
+            }
+        }
+
+        #endregion UI Methods
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+Gen. II Kinect 4 Windows – Template
+===========
+Template for **G2K Basic overview** tutorial that illustrate the base functionality of the second Kinect for Windows.
+
+
+![K4W logo](http://www.kinectingforwindows.com/wp-content/themes/twentyten/images/headers/logo.jpg)
